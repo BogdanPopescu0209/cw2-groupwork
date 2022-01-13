8665c5bce31c7532493b68a45a64e32dc3f9f99e
BogdanPopescu0209 <59745574+BogdanPopescu0209@users.noreply.github.com>
2022-01-13 18:54:47 +0000

    Add route /lessons

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 
 const app = express();
 
+app.get('/lessons', function(request, response){
+    response.send([
+        { 'topic': 'math', 'location': 'London', 'price': 100 },
+        { 'topic': 'math', 'location': 'Liverpool', 'price': 80 },
+        { 'topic': 'math', 'location': 'Oxford', 'price': 90 },
+        { 'topic': 'math', 'location': 'Bristol', 'price': 120 },
+       ])
+})
+
 app.use(function (request, response){
     response.status(404).send("Page Not Found!")
 })
